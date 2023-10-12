a1cf9dca1e0bb2092c917400b0db86adedb1033d
Carsten Ringe <carsten.ringe@alphabrik.com>
2023-10-12 13:45:07 +0200

    Override Attachment#equals and hashCode

--- src/main/java/com/alphabrik/msal/model/Attachment.java
+++ src/main/java/com/alphabrik/msal/model/Attachment.java
@@ -3,6 +3,7 @@ package com.alphabrik.msal.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.time.Instant;
+import java.util.Objects;
 
 public record Attachment(
     @JsonProperty("@odata.type")
@@ -13,6 +14,21 @@ public record Attachment(
     byte[] contentBytes,
     Instant lastModifiedDateTime
 ) {
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (!(o instanceof final Attachment that)) return false;
+        return Objects.equals(type, that.type) && Objects.equals(id, that.id) && Objects.equals(
+            name,
+            that.name
+        ) && Objects.equals(contentType, that.contentType) && Objects.equals(lastModifiedDateTime, that.lastModifiedDateTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, id, name, contentType, lastModifiedDateTime);
+    }
 
     @Override
     public String toString() {
